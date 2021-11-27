8ef5f94d7f2faf9398b0fc428c5926426f76d8a8
v.shevchuk <v.shevchuk@viseven.com>
2021-11-27 18:09:00 +0200

    added basics components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
-function App() {
-  return <div className="App"></div>;
+import { Component } from 'react';
+
+import { generate } from 'shortid';
+
+import Form from 'components/Form';
+import Filter from 'components/Filter';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+class App extends Component {
+  state = { contacts: initialContacts, filter: '' };
+
+  addContact = (name, number) => {
+    const contact = { id: generate(), name, number };
+
+    this.setState((prevState) => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  createContact = ({ id, name, number }) => {
+    return (
+      <li key={id}>
+        <p>{name}</p>
+        <p>{number}</p>
+      </li>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <Form onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <h3>Find contacts by name</h3>
+        <Filter />
+        <ul>{this.state.contacts.map(this.createContact)}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,61 @@
+import { Component } from 'react';
+
+import { generate } from 'shortid';
+
+class Form extends Component {
+  state = { name: '', number: '' };
+
+  nameInputId = generate();
+  numberInputId = generate();
+
+  handleChange = ({ currentTarget: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  handleSubmit = (event) => {
+    const {
+      props,
+      state: { name, number },
+      reset,
+    } = this;
+    event.preventDefault();
+    props.onSubmit(name, number);
+    reset();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor={this.nameInputId}>Name</label>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={this.state.name}
+          onChange={this.handleChange}
+          id={this.nameInputId}
+        />
+        <label htmlFor={this.numberInputId}>Number</label>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={this.state.number}
+          onChange={this.handleChange}
+          id={this.numberInputId}
+        />
+        <button type="submit">Add Contact</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
