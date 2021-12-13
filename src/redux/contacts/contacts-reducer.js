f2a4997d8ac8419b3ae2adbc0f1cb2c828e335e4
v.shevchuk <v.shevchuk@viseven.com>
2021-12-13 09:34:52 +0200

    slice with spread

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,8 +1,4 @@
-import {
-  createReducer,
-  combineReducers,
-  // createSlice
-} from '@reduxjs/toolkit';
+import { createReducer, combineReducers, createSlice } from '@reduxjs/toolkit';
 import {
   changeFilter,
   // addContactRequest,
@@ -22,55 +18,98 @@ import {
   deleteContact,
 } from './contacts-operations';
 
-// const itemsSlice = createSlice({
-//   name: 'items',
-//   initialState: { items: [], filter: '', error: null },
-//   extraReducers: {
-//     [fetchContacts.fulfilled]: (state, action) => ({
-//       ...state,
-//       items: action.payload,
-//     }),
-//     [fetchContacts.pending]: (state) => ({ ...state, isLoading: true }),
-//     [fetchContacts.rejected]: (state, action) =>
-//       (state.error = action.error.message),
-//   },
+const itemsSlice = createSlice({
+  name: 'items',
+  initialState: { items: [], loading: false, filter: '', error: null },
+  extraReducers: {
+    [fetchContacts.fulfilled]: (state, { payload }) => ({
+      ...state,
+      items: payload,
+      loading: false,
+      error: null,
+    }),
+    [fetchContacts.pending]: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    [fetchContacts.rejected]: (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.error.message,
+    }),
+
+    [addContact.fulfilled]: (state, { payload }) => ({
+      ...state,
+      items: [payload, ...state.items],
+      loading: false,
+      error: null,
+    }),
+    [addContact.pending]: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    [addContact.rejected]: (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.error.message,
+    }),
+
+    [deleteContact.fulfilled]: (state, { payload }) => ({
+      ...state,
+      items: state.items.filter(({ id }) => id !== payload.id),
+      loading: false,
+      error: null,
+    }),
+    [deleteContact.pending]: (state) => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    [deleteContact.rejected]: (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.error.message,
+    }),
+  },
+});
+
+// const items = createReducer([], {
+//   // [fetchContacts.fulfilled]: (_, { payload }) => payload,
+//   // [addContact.fulfilled]: (prevState, { payload }) => [payload, ...prevState],
+//   // [deleteContact.fulfilled]: (prevState, { payload }) =>
+//   //   prevState.filter(({ id }) => id !== payload.id),
 // });
 
-const items = createReducer([], {
-  [fetchContacts.fulfilled]: (_, { payload }) => payload,
-  [addContact.fulfilled]: (prevState, { payload }) => [payload, ...prevState],
-  [deleteContact.fulfilled]: (prevState, { payload }) =>
-    prevState.filter(({ id }) => id !== payload.id),
-});
+// const loading = createReducer(false, {
+//   // [addContact.pending]: () => true,
+//   // [addContact.fulfilled]: () => false,
+//   // [addContact.rejected]: () => false,
+//   // [deleteContact.pending]: () => true,
+//   // [deleteContact.fulfilled]: () => false,
+//   // [deleteContact.rejected]: () => false,
+//   // [fetchContacts.pending]: () => true,
+//   // [fetchContacts.fulfilled]: () => false,
+//   // [fetchContacts.rejected]: () => false,
+// });
 
-const loading = createReducer(false, {
-  [addContact.pending]: () => true,
-  [addContact.fulfilled]: () => false,
-  [addContact.rejected]: () => false,
-  [deleteContact.pending]: () => true,
-  [deleteContact.fulfilled]: () => false,
-  [deleteContact.rejected]: () => false,
-  [fetchContacts.pending]: () => true,
-  [fetchContacts.fulfilled]: () => false,
-  [fetchContacts.rejected]: () => false,
-});
+// const filter = createReducer('', {
+//   [changeFilter]: (_, { payload }) => payload,
+// });
 
-const filter = createReducer('', {
-  [changeFilter]: (_, { payload }) => payload,
-});
+// const error = createReducer(null, {
+//   // [addContact.rejected]: (_, { payload }) => payload,
+//   // [deleteContact.rejected]: (_, { payload }) => payload,
+//   // [fetchContacts.rejected]: (_, { payload }) => payload,
+//   // [addContact.pending]: () => null,
+//   // [addContact.fulfilled]: () => null,
+//   // [deleteContact.pending]: () => null,
+//   // [deleteContact.fulfilled]: () => null,
+//   // [fetchContacts.pending]: () => null,
+//   // [fetchContacts.fulfilled]: () => null,
+// });
 
-const error = createReducer(null, {
-  [addContact.rejected]: (_, { payload }) => payload,
-  [deleteContact.rejected]: (_, { payload }) => payload,
-  [fetchContacts.rejected]: (_, { payload }) => payload,
-  [addContact.pending]: () => null,
-  [addContact.fulfilled]: () => null,
-  [deleteContact.pending]: () => null,
-  [deleteContact.fulfilled]: () => null,
-  [fetchContacts.pending]: () => null,
-  [fetchContacts.fulfilled]: () => null,
-});
+// export default combineReducers({ items, filter, loading, error });
 
-export default combineReducers({ items, filter, loading, error });
-
-// export default itemsSlice.reducer;
+export default itemsSlice.reducer;
