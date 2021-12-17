39fb9a9575d473abd22be4727db51b9908a71ee1
Victor-page <victor.shevchuk.96@gmail.com>
2021-12-17 13:05:00 +0300

    Updates

---
+++ static/js/6.b8f6ec01.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpphonebook=this.webpackJsonpphonebook||[]).push([[6],{110:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return a}));var i=t(96),o=t.n(i),c=t(2),a=function(){return Object(c.jsx)("div",{className:o.a.container,children:Object(c.jsxs)("h1",{className:o.a.title,children:["Welcome!"," ",Object(c.jsx)("span",{role:"img","aria-label":"Welcome icon",children:"\ud83d\udc4b"})]})})}},96:function(e,n,t){e.exports={container:"HomeView_container__KpowT",title:"HomeView_title__2Qibb"}}}]);
+//# sourceMappingURL=6.b8f6ec01.chunk.js.map
